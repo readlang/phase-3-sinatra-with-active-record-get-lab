3b29cc6c8f1e0b38cf06cb95d359eb7d82d094a1
Read Langworthy <rlangworthy@gmail.com>
2022-03-09 16:12:04 -0800

    commit tree 211423d2437983d47ac7bf9ed9208522aee2d27b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc )
+    baked_goods.to_json()
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order(price: :desc).limit(1)[0]
+    baked_goods.to_json(only: [:name, :price])
+  end
+
 
 end
+
+
+# GET /baked_goods/by_price: returns an array of baked goods as JSON, sorted by price in descending order. (HINT: how can you use Active Record to sort the baked goods in a particular order?)
+
+# GET /baked_goods/most_expensive: returns the single most expensive baked good as JSON. (HINT: how can you use Active Record to sort the baked goods in a particular order?)
